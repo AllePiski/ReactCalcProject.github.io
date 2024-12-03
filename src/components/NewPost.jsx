4fa9c8cb5654d2de2ad0c2aa658ab5ef74399a3a
Alessandro Piscitiello <alessandro.piscitiello@webion.com>
2024-12-03 15:50:54 +0100

    added mainheader and modal for the form

--- src/components/NewPost.jsx
+++ src/components/NewPost.jsx
@@ -1,23 +1,15 @@
 import classes from "./newPost.module.css";
-import { useState } from "react";
 
-export default function NewPost() {
-  const [enteredBody, setEnteredBody] = useState("");
-
-  const changeBodyHeader = (event) => {
-    setEnteredBody(event.target.value);
-  };
-
+export default function NewPost(props) {
   return (
     <form className={classes.form}>
       <p>
         <label htmlFor="body">Text</label>
-        <textarea id="body" required rows={3} onChange={changeBodyHeader} />
+        <textarea id="body" required rows={3} onChange={props.onBodyChange} />
       </p>
-      <p>{enteredBody}</p>
       <p>
         <label htmlFor="name">Your name</label>
-        <input type="text" id="name" required />
+        <input type="text" id="name" required onChange={props.onAuthorChange} />
       </p>
     </form>
   );
--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,14 +1,39 @@
 import Post from "./Post";
 import classes from "./Post.module.css";
 import NewPost from "./NewPost";
+import Modal from "./Modal";
+import { useState } from "react";
 
 export default function PostList() {
+  const [enteredBody, setEnteredBody] = useState("");
+  const [enteredAuthor, setEnteredAuthor] = useState("");
+  const [isModalVisible, setIsModalVisible] = useState(true);
+
+  const bodyChangeHandler = (event) => {
+    setEnteredBody(event.target.value);
+  };
+
+  const AuthorChangeHandler = (event) => {
+    setEnteredAuthor(event.target.value);
+  };
+
+  const hideModalHandler = () => {
+    setIsModalVisible(false);
+  };
+
   return (
     <>
-      <NewPost />
+      {isModalVisible && (
+        <Modal onClose={hideModalHandler}>
+          <NewPost
+            onBodyChange={bodyChangeHandler}
+            onAuthorChange={AuthorChangeHandler}
+          />
+        </Modal>
+      )}
+
       <ul className={classes.posts}>
-        <Post author={"qewr"} body={"qwer"} />
-        <Post author={"yy"} body={"qwyyyer"} />
+        <Post author={enteredAuthor} body={enteredBody} />
       </ul>
     </>
   );
